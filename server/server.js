b2433de881a0b83a8b36010bd49c9f0e94215b07
satyam-mishra-07 <satyammishrakumar39@gmail.com>
2025-08-30 18:22:12 +0530

    change

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ const uri = process.env.MONGODB_URI;
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname, "../client/dist")))
 
-  app.get("/.*/", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
   })
 }
