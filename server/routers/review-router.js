cdf248774969a0ec6c9718bde5761f42e53aa9a2
satyam-mishra-07 <satyammishrakumar39@gmail.com>
2025-09-02 15:35:13 +0530

    change

--- server/routers/review-router.js
+++ server/routers/review-router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth-middleware.js';
-import reviewController, { uploadFields } from "./controllers/review-controller.js";
+import reviewController, { uploadFields } from "../controllers/review-controller.js";
 
 const router = express.Router();
 
